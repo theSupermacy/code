7b2faeae5987d7abc1f4b030053116c676b6a23e
supermacy <rahulanandsinha@gmail.com>
2016-08-25 11:21:32 +0530

    pending work of down to zero

--- downtoZero2.cpp
+++ downtoZero2.cpp
@@ -1,49 +1,38 @@
 /* This code is for question in HackerRank,The question name is Left Rotation*/
-
 #include <iostream>
 #include <cmath>
-
 int SIZE= 1000000+1;
+//int SIZE= 10;
 int primeArr[1000001];
+int primeArr2[1000001];
 using namespace std;
 void calculatePrime();
 int isPrime(int n);
-int getMax(long int n);
-
-
-
-
 int main()
 {
 	int foobar[SIZE];
 	for(int i = 0 ; i < SIZE;i++)
-	{	primeArr[i]=0;
+	{	primeArr[i]=1;
+		primeArr2[i]=1;
 		foobar[i]=0;
 	}
 	calculatePrime();
-	//for(int i =0;i<100;i++)
-	//cout<<primeArr[i]<<" "<<i<<endl;
-//		int foobar[SIZE];
-//	for(int i = 0 ; i < SIZE;i++)
-  //              foobar[i]=0;
 	foobar[0]=0;
 	for(int i =1; i<SIZE;i++)
 	{
-		long int n;
-//		cout<<isPrime(i)<<"testing"<<endl;
-		if(!isPrime(i))
-			n=foobar[i-1]+1;
+	 	int x= primeArr[i];
+		int y= primeArr2[i];
+		x=min(x,y);
+//		cout<<x<<"testinig"<<i<<endl;
+		if(x==1)
+		foobar[i]=foobar[i-1];
 		else
-		{
-			
-			int c=getMax(i);
-//			cout<<c<<"c hain bc"<<i<<endl;
-			n=foobar[c]+1;
-		}
-		foobar[i]=n;
+			foobar[i]=min(min(foobar[max(x,i/x)],foobar[max(y,i/y)]),foobar[i-1]);
+		foobar[i]=foobar[i]+1;
 	}
-//	for(int i =0; i<100;i++)
-//	cout<<foobar[i]<<" "<<i<<endl;
+	cout<<primeArr[556]<<" "<<primeArr2[556];
+	// for(int i =0; i<10;i++)
+	// cout<<foobar[i]<<" "<<i<<endl;
 	int q;
 	cin>>q;
 	for(int i = 0; i< q;i++)
@@ -52,47 +41,22 @@ int main()
 		cin>>n;
 		cout<<foobar[n]<<endl;
 	}
-			
+
 	return 0;
 }
-void calculatePrime()
-        {
-        primeArr[0]=1;
-        primeArr[1]=0;
-        for (int i=2;i*i<=SIZE;i++)
-                {
-        //      cout<<i;
-                        if(!primeArr[i])
-                        for(int j=2;j*i<SIZE;j++)
-                       {
-			if()
-				 primeArr[j*i]=i*j;
- 
-                }
+void calculatePrime(){
+	primeArr[0]=0;
+	primeArr2[0]=0;
+	for (int i=2;i*i<=SIZE;i++){
+  	for(int j=2;j*i<SIZE;j++){
+			primeArr[j*i]=i;
+			if(primeArr2[i]==1)
+				primeArr2[j*i]=i;
+		}
+	}
 }
 
 int isPrime(int n){
 //cout<<n<<"testiiii"<<primeArr[n]<<endl;
 return primeArr[n];
 }
-
-int getMax(long int i)
-{
-        int n,k,l=(int)sqrt(i);
-  //      cout<<l<<"test"<<i<<endl;
-        for(int  j =l;j>0;j--)
-        {
-                if(i%j==0)
-                {
-                n=j;
-                k=i/j;
-                break;
-//		cout<<j<<"break"<<endl;
-                }
-        }
-//	cout<<n<<k<<endl;
-        n=(n>=k)?n:k;
-        return n ;
-}
-
-
